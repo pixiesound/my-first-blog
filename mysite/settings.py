7b82248c322490589d8da4ceaa3e8a9ce31e2672
Your Name <claudia@pixiesound.com>
2017-06-17 16:36:36 +0200

    added name to pythonanywhere settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+6yyp8bep$(!3#oyq9@o+zckx#dr4xf-t!)31h99qi+27t9r&#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pixiesound.pythonanywhere.com']
 
 
 # Application definition
